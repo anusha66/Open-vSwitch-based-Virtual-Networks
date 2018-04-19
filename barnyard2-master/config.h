019223adf8fe8b81b9e04451b4189c63d7a8e771
Anusha Prakash <anushap@andrew.cmu.edu>
2018-04-19 14:09:11 -0400

    packages added
    Required packages added

---
+++ barnyard2-master/config.h
@@ -0,0 +1,288 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define if building universal (internal helper macro) */
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
+
+/* Define if AIX */
+/* #undef AIX */
+
+/* Define if broken SIOCGIFMTU */
+/* #undef BROKEN_SIOCGIFMTU */
+
+/* Define if BSDi */
+/* #undef BSDI */
+
+/* Define if CYGWIN */
+/* #undef CYGWIN */
+
+/* Define if errlist is predefined */
+#define ERRLIST_PREDEFINED 1
+
+/* Define if FreeBSD */
+/* #undef FREEBSD */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if the system has the type `int16_t'. */
+#define HAVE_INT16_T 1
+
+/* Define to 1 if the system has the type `int32_t'. */
+#define HAVE_INT32_T 1
+
+/* Define to 1 if the system has the type `int64_t'. */
+#define HAVE_INT64_T 1
+
+/* Define to 1 if the system has the type `int8_t'. */
+#define HAVE_INT8_T 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `crypto' library (-lcrypto). */
+/* #undef HAVE_LIBCRYPTO */
+
+/* Define to 1 if you have the `curl' library (-lcurl). */
+/* #undef HAVE_LIBCURL */
+
+/* Define to 1 if you have the `json' library (-ljson). */
+/* #undef HAVE_LIBJSON */
+
+/* Define to 1 if you have the `m' library (-lm). */
+#define HAVE_LIBM 1
+
+/* Define to 1 if you have the `nsl' library (-lnsl). */
+#define HAVE_LIBNSL 1
+
+/* Define to 1 if you have the `pcap' library (-lpcap). */
+#define HAVE_LIBPCAP 1
+
+/* Define to 1 if you have the `pfring' library (-lpfring). */
+/* #undef HAVE_LIBPFRING */
+
+/* Define to 1 if you have the `pq' library (-lpq). */
+/* #undef HAVE_LIBPQ */
+
+/* Define whether Prelude support is enabled */
+/* #undef HAVE_LIBPRELUDE */
+
+/* Define to 1 if you have the `socket' library (-lsocket). */
+/* #undef HAVE_LIBSOCKET */
+
+/* Define to 1 if you have the `websockets' library (-lwebsockets). */
+/* #undef HAVE_LIBWEBSOCKETS */
+
+/* Define to 1 if you have the `wpcap' library (-lwpcap). */
+/* #undef HAVE_LIBWPCAP */
+
+/* Define to 1 if you have the `z' library (-lz). */
+#define HAVE_LIBZ 1
+
+/* Define whether linuxthreads is being used */
+/* #undef HAVE_LINUXTHREADS */
+
+/* Define to 1 if you have the <math.h> header file. */
+#define HAVE_MATH_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <paths.h> header file. */
+#define HAVE_PATHS_H 1
+
+/* Define to 1 if you have the <pcap.h> header file. */
+#define HAVE_PCAP_H 1
+
+/* Define to 1 if you have the <pfring.h> header file. */
+/* #undef HAVE_PFRING_H */
+
+/* Define if PQping exists. */
+/* #undef HAVE_PQPING */
+
+/* Define to 1 if you have the `snprintf' function. */
+#define HAVE_SNPRINTF 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strerror' function. */
+#define HAVE_STRERROR 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strlcat' function. */
+/* #undef HAVE_STRLCAT */
+
+/* Define to 1 if you have the `strlcpy' function. */
+/* #undef HAVE_STRLCPY */
+
+/* Define to 1 if you have the `strtoul' function. */
+/* #undef HAVE_STRTOUL */
+
+/* Define to 1 if you have the <sys/sockio.h> header file. */
+/* #undef HAVE_SYS_SOCKIO_H */
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if the system has the type `uint16_t'. */
+#define HAVE_UINT16_T 1
+
+/* Define to 1 if the system has the type `uint32_t'. */
+#define HAVE_UINT32_T 1
+
+/* Define to 1 if the system has the type `uint64_t'. */
+#define HAVE_UINT64_T 1
+
+/* Define to 1 if the system has the type `uint8_t'. */
+#define HAVE_UINT8_T 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if the system has the type `u_int16_t'. */
+#define HAVE_U_INT16_T 1
+
+/* Define to 1 if the system has the type `u_int32_t'. */
+#define HAVE_U_INT32_T 1
+
+/* Define to 1 if the system has the type `u_int64_t'. */
+#define HAVE_U_INT64_T 1
+
+/* Define to 1 if the system has the type `u_int8_t'. */
+#define HAVE_U_INT8_T 1
+
+/* Define to 1 if you have the `vsnprintf' function. */
+/* #undef HAVE_VSNPRINTF */
+
+/* Define to 1 if you have the `vswprintf' function. */
+#define HAVE_VSWPRINTF 1
+
+/* Define to 1 if you have the <wchar.h> header file. */
+#define HAVE_WCHAR_H 1
+
+/* Define to 1 if you have the `wprintf' function. */
+#define HAVE_WPRINTF 1
+
+/* Define if the compiler understands __FUNCTION__. */
+#define HAVE___FUNCTION__ 1
+
+/* Define if the compiler understands __func__. */
+/* #undef HAVE___func__ */
+
+/* Define if HP-UX 10 or 11 */
+/* #undef HPUX */
+
+/* For INADDR_NONE definition */
+/* #undef INADDR_NONE */
+
+/* Define if Irix 6 */
+/* #undef IRIX */
+
+/* Define if Linux */
+#define LINUX 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Define if MacOS */
+/* #undef MACOS */
+
+/* For MySQL versions 5.0.13 and greater */
+#define MYSQL_HAS_OPT_RECONNECT 1
+
+/* For MySQL versions 5.0.13 to 5.0.18 */
+/* #undef MYSQL_HAS_OPT_RECONNECT_BUG */
+
+/* Define if OpenBSD < 2.3 */
+/* #undef OPENBSD */
+
+/* Define if Tru64 */
+/* #undef OSF1 */
+
+/* Name of package */
+#define PACKAGE "barnyard2"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "barnyard2"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "barnyard2 1.14"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "barnyard2"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.14"
+
+/* Define if pcap timeout is ignored */
+#define PCAP_TIMEOUT_IGNORED 1
+
+/* Define whether application use libtool >= 2.0 */
+/* #undef PRELUDE_APPLICATION_USE_LIBTOOL2 */
+
+/* The size of `char', as computed by sizeof. */
+#define SIZEOF_CHAR 1
+
+/* The size of `int', as computed by sizeof. */
+#define SIZEOF_INT 4
+
+/* The size of `long int', as computed by sizeof. */
+#define SIZEOF_LONG_INT 8
+
+/* The size of `long long int', as computed by sizeof. */
+#define SIZEOF_LONG_LONG_INT 8
+
+/* The size of `short', as computed by sizeof. */
+#define SIZEOF_SHORT 2
+
+/* The size of `unsigned int', as computed by sizeof. */
+#define SIZEOF_UNSIGNED_INT 4
+
+/* The size of `unsigned long int', as computed by sizeof. */
+#define SIZEOF_UNSIGNED_LONG_INT 8
+
+/* The size of `unsigned long long int', as computed by sizeof. */
+#define SIZEOF_UNSIGNED_LONG_LONG_INT 8
+
+/* Define if Solaris */
+/* #undef SOLARIS */
+
+/* For sparc v9 with %time register */
+/* #undef SPARCV9 */
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define if SunOS */
+/* #undef SUNOS */
+
+/* Version number of package */
+#define VERSION "1.14"
+
+/* Define if words are big endian */
+/* #undef WORDS_BIGENDIAN */
+
+/* Define if words must align */
+/* #undef WORDS_MUSTALIGN */
+
+/* Define __FUNCTION__ as required. */
+/* #undef __FUNCTION__ */
